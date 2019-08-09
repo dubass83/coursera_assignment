9f55ee6e77e9e96186a0d98aae7ef7a0d54f5c56
dubass83 <makssych@gmail.com>
2019-08-09 22:38:37 +0300

    Done first part

--- template/views.py
+++ template/views.py
@@ -1,10 +1,46 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_protect
 
 
 # Create your views here.
-
+@csrf_exempt
+# @csrf_protect
+@require_http_methods(["GET", "POST"])
 def echo(request):
-    pass
+    # response = HttpResponse()
+    if request.method == 'GET' and request.GET:
+        key = [key for key, item in request.GET.dict().items()][0]
+        return render(request, 'echo.html', context={
+            'has_body': True,
+            'method': 'get',
+            'q_str': "{}: {}".format(key, request.GET[key]),
+            'now': 'empty'
+        })
+    elif request.method == 'POST' and request.POST:
+        # print(request.POST)
+        key = [key for key, item in request.POST.dict().items()][0]
+        return render(request, 'echo.html', context={
+            'has_body': True,
+            'method': 'post',
+            'q_str': "{}: {}".format(key, request.POST[key]),
+            'now': 'empty'
+        })
+    else:
+        if 'http_X_Print_Statement'.upper() in request.META.keys():
+            return render(request, 'echo.html', context={
+                'has_body': False,
+                # 'method': 'post',
+                'now': request.META['http_X_Print_Statement'.upper()]
+            })
+        # print(request.META.keys())
+        return render(request, 'echo.html', context={
+            'has_body': False,
+            # 'method': 'post',
+            'now': 'empty'
+        })
 
 
 def filters(request):
