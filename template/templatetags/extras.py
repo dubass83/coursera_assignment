45c472482aa162d57a0dc4c0e9dd6933f1c081e9
dubass83 <makssych@gmail.com>
2019-08-10 21:46:51 +0300

    Done second part

--- template/templatetags/extras.py
+++ template/templatetags/extras.py
@@ -1,1 +1,26 @@
 from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def inc(value, args):
+    """
+    :param value:
+    :param args:
+    :return: increment value plus args
+    """
+    return int(value) + int(args)
+
+@register.simple_tag
+def division(a, b, to_int=False):
+    """
+    :param a: int or float
+    :param b: int or float
+    :param to_int: True or False
+    :return:
+    """
+    if to_int:
+        return int(a) // int(b)
+    return int(a) / int(b)
