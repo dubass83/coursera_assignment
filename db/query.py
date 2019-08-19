6bbf2a691e34185f9b2f524920ee72a2ed127fef
dubass83 <makssych@gmail.com>
2019-08-19 20:23:55 +0300

    Edit db/query.py finish first db task

--- db/query.py
+++ db/query.py
@@ -7,8 +7,38 @@ from db.models import User, Blog, Topic
 
 
 def create():
-    pass
+    u1 = User(first_name="u1", last_name="u1")
+    u2 = User(first_name="u2", last_name="u2")
+    u3 = User(first_name="u3", last_name="u3")
 
+    u1.save()
+    u2.save()
+    u3.save()
+
+    blog1 = Blog(id=None, title="blog1", author=u1)
+    blog2 = Blog(id=None, title="blog2", author=u1)
+
+    blog1.save()
+    blog2.save()
+
+    blog1.subscribers.add(u1)
+    blog1.subscribers.add(u2)
+    blog2.subscribers.add(u2)
+
+    blog1.save()
+    blog2.save()
+
+    topic1 = Topic(title="topic1", blog=blog1, author=u1)
+    topic2 = Topic(title="topic2_content", blog=blog1, author=u3, created=datetime(2017, 1, 1, tzinfo=UTC))
+
+    topic1.save()
+    topic2.save()
+
+    topic1.likes.add(u1)
+    topic1.likes.add(u2)
+    topic1.likes.add(u3)
+
+    topic1.save()
 
 def edit_all():
     pass
@@ -64,3 +94,7 @@ def get_topic_that_like_all_users():
 
 def get_topic_that_dont_have_like():
     pass
+
+
+if __name__ == "__main__":
+    create()
