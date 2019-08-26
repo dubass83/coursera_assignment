89305b44ca1505be1100bb0f8c635e257eff540d
dubass83 <makssych@gmail.com>
2019-08-26 22:06:10 +0300

    Finish db task

--- db/query.py
+++ db/query.py
@@ -21,8 +21,8 @@ def create():
     blog1.save()
     blog2.save()
 
-    blog1.subscribers.add(u1)
-    blog1.subscribers.add(u2)
+    blog1.subscribers.add(u1, u2)
+    # blog1.subscribers.add(u2)
     blog2.subscribers.add(u2)
 
     blog1.save()
@@ -34,9 +34,9 @@ def create():
     topic1.save()
     topic2.save()
 
-    topic1.likes.add(u1)
-    topic1.likes.add(u2)
-    topic1.likes.add(u3)
+    topic1.likes.add(u1, u2, u3)
+    # topic1.likes.add(u2)
+    # topic1.likes.add(u3)
 
     topic1.save()
 
@@ -58,43 +58,47 @@ def edit_u1_u2():
 
 
 def delete_u1():
-    pass
+    User.objects.filter(first_name='u1').delete()
 
 
 def unsubscribe_u2_from_blogs():
-    pass
+    user2 = User.objects.filter(first_name="u2")
+    blog_with_user2 = Blog.objects.filter(subscribers__first_name="u2")
+    for blog in blog_with_user2:
+        for user in user2:
+            blog.subscribers.remove(user)
 
 
 def get_topic_created_grated():
-    pass
+    return Topic.objects.filter(created__gt=datetime.datetime(2018, 1, 1, tzinfo=UTC))
 
 
 def get_topic_title_ended():
-    pass
+    return Topic.objects.filter(title__endswith='content')
 
 
 def get_user_with_limit():
-    pass
+    return User.objects.all().order_by("-id")[0:2]
 
 
 def get_topic_count():
-    pass
+    return Topic.objects.all().values('blog').annotate(total=Count('blog')).order_by('total')
 
 
 def get_avg_topic_count():
-    pass
+    return Topic.objects.all().values('blog').annotate(total=Count('blog')).aggregate(Avg('total'))
 
 
 def get_blog_that_have_more_than_one_topic():
-    pass
+    return Topic.objects.values('blog_id').annotate(total=Count('id')).filter(total__gt=1)
 
 
 def get_topic_by_u1():
-    pass
+    return Topic.objects.select_related('author').filter(author__first_name='u1')
 
 
 def get_user_that_dont_have_blog():
-    pass
+    return User.objects.filter(blog__isnull=True).values_list('id').order_by('id')
 
 
 def get_topic_that_like_all_users():
@@ -102,7 +106,7 @@ def get_topic_that_like_all_users():
 
 
 def get_topic_that_dont_have_like():
-    pass
+    return Topic.objects.filter(likes__isnull=True)
 
 
 if __name__ == "__main__":
