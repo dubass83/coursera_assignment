42bef64c01ff4b34add931b44f12ba3df1f7577f
dubass83 <makssych@gmail.com>
2019-10-15 22:09:48 +0300

    Edit views.py, add index and topics_details to views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'core/index.html')
+
+
+def topic_details(request, pk):
+    return render(request, 'core/topic_details.html')
