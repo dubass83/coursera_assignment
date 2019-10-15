6c075365dfce8be19ff75ce17b1d7d1a5d14699b
dubass83 <makssych@gmail.com>
2019-10-15 22:09:03 +0300

    Edit urls.py, add index and topics_details to urls

--- grader/urls.py
+++ grader/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from core.views import index, topic_details
 
 urlpatterns = [
+    url(r'^index$', index, name='index'),
+    url(r'^topic/(?P<pk>\d+)/$', topic_details, name='topic_details'),
     url(r'^routing/', include('routing.urls')),
     url(r'^template/', include('template.urls')),
 ]
